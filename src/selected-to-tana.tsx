3f8de337a9bd41914717536fb0621bd3c8e5673d
Lisa Ross <lisa@avenuego.com>
2025-05-24 18:22:35 -0400

    feat: enhance Tana conversion by including active browser tab URL and title in output

--- src/selected-to-tana.tsx
+++ src/selected-to-tana.tsx
@@ -1,4 +1,4 @@
-import { showHUD, getSelectedText, Clipboard } from '@raycast/api'
+import { showHUD, getSelectedText, Clipboard, BrowserExtension } from '@raycast/api'
 import { convertToTana } from './utils/tana-converter'
 import { exec } from 'child_process'
 import { promisify } from 'util'
@@ -23,8 +23,39 @@ export default async function Command() {
       return
     }
 
+    // Try to get the active browser tab URL
+    let urlField = ''
+    let pageTitle = ''
+    try {
+      const tabs = await BrowserExtension.getTabs()
+      const activeTab = tabs?.find((tab) => tab.active && tab.url?.startsWith('http'))
+      if (activeTab && activeTab.url) {
+        urlField = `\nURL::${activeTab.url}`
+        // Try to get the page title
+        try {
+          pageTitle = await BrowserExtension.getContent({
+            cssSelector: 'title',
+            format: 'text',
+            tabId: activeTab.id,
+          })
+        } catch { /* ignore error */ }
+      }
+    } catch { /* ignore error */ }
+
+    // Format output: if pageTitle, use as parent node
+    let textWithUrl = selectedText + urlField
+    if (pageTitle) {
+      // Split selectedText into lines and indent each
+      const selectedLines = selectedText.split('\n').map((line) => '  ' + line)
+      // Place URL as the first child if present
+      const urlLine = urlField ? '  ' + urlField.trim() : ''
+      // Combine URL and selected text lines
+      const indented = [urlLine, ...selectedLines].filter(Boolean).join('\n')
+      textWithUrl = `# ${pageTitle} #swipe\n${indented}`
+    }
+
     // Convert to Tana format
-    const tanaOutput = convertToTana(selectedText)
+    const tanaOutput = convertToTana(textWithUrl)
 
     // Copy to clipboard
     await Clipboard.copy(tanaOutput)
